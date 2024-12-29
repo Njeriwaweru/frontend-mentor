322c88f83a4ea5406be236a317626d13cd35ae9b
Njeriwaweru <njeripraise318@gmail.com>
2024-12-29 22:17:07 +0300

    Adding time tracking dashboard

---
+++ time-tracking-dashboard/JS/script.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const content = document.getElementById("content");
+    const buttons = document.querySelectorAll(".header--list button")
+
+    async function populate() {
+        const requestURL = "data.json";
+        const request = new Request(requestURL);
+
+        try {
+            const response = await fetch(request);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            buttons.forEach((button) => {
+                button.addEventListener("click", () => {
+                    const timeframe = button.textContent.toLowerCase();
+                    populateCards(data, timeframe)
+                });
+            });
+            populateCards(data, "daily")
+        } catch (error) {
+            console.error("Error fetching the data:", error);
+        } 
+    }
+
+    const populateCards = (data, timeframe) => {
+        content.innerHTML='';
+
+        data.forEach((item) =>{
+            const card = document.createElement("div");
+            const sanitizedTitle = item.title.toLowerCase().replace(/\s+/g, '-');
+            card.classList.add("card", sanitizedTitle);
+
+            const cardContent = document.createElement("div");
+            cardContent.classList.add("cardContent");
+
+            const titleAndImageWrapper = document.createElement("div");
+            titleAndImageWrapper.classList.add("title-image-wrapper");
+
+            const title = document.createElement("h2");
+            title.textContent = item.title;
+
+            const image = document.createElement("img")
+            image.src = `images/icon-ellipsis.svg`;
+            image.alt = item.title
+            image.classList.add("card-image");
+
+            titleAndImageWrapper.appendChild(title);
+            titleAndImageWrapper.appendChild(image);
+            cardContent.appendChild(titleAndImageWrapper);
+
+            const times = item.timeframes[timeframe];
+            const timeframeDiv = document.createElement("div");
+            timeframeDiv.classList.add("timeframe");
+
+            const current = document.createElement("p");
+            current.textContent = `${times.current}hrs`;
+
+            const previous = document.createElement("p");
+            const span = document.createElement("span");
+            span.textContent = `Last Week - ${times.previous}hrs`;
+            previous.appendChild(span);
+
+            timeframeDiv.appendChild(current);
+            timeframeDiv.appendChild(previous);
+            cardContent.appendChild(timeframeDiv);
+
+            card.appendChild(cardContent);
+
+            content.appendChild(card);
+        })
+    }
+
+    populate();
+})
---
+++ time-tracking-dashboard/README.md
@@ -0,0 +1,18 @@
+# Frontend Mentor - Time tracking dashboard
+
+![Design preview for the Time tracking dashboard coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+# Links
+Live site []
+
+# Author
+Praise Waweru
+
+The users are be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
